e9f5efb7977516b0986676990dc2cfcc962ee664
BrunoGomes22 <brunofgomes@ua.pt>
2024-12-08 19:55:40 +0000

    small tweak in avx512f algorithm

--- deti_coins_cpu_avx512f_search.h
+++ deti_coins_cpu_avx512f_search.h
@@ -13,7 +13,7 @@ static void deti_coins_cpu_avx512_search(void)
     u32_t n, idx, coin[13u], hash[4u];
     u64_t n_attempts, n_coins;
     u08_t *bytes;
-    static u32_t interleaved_data[13u * 16u] __attribute__((aligned(64))); // 16 DETI coins
+    static u32_t interleaved_coins[13u * 16u] __attribute__((aligned(64))); // 16 DETI coins
     static u32_t interleaved_hash[4u * 16u] __attribute__((aligned(64)));   // 16 MD5 hashes
 
     bytes = (u08_t *)&coin[0]; // acesses coin information byte per byte
@@ -33,7 +33,37 @@ static void deti_coins_cpu_avx512_search(void)
     //
     // arbitrary, but printable utf-8 data terminated with a '\n' is highly desirable
     //
-    for (idx = 10u; idx < 13u * 4u - 1u; idx++) // to do: generate random ASCII symbols
+    u32_t random_num = rand() % 10000;
+
+    char random_str[5];
+    snprintf(random_str, sizeof(random_str), "%04u", random_num);
+
+    bytes[10u] = ' ';
+    bytes[11u] = ' ';
+    bytes[12u] = ' ';
+    bytes[13u] = ' ';
+    bytes[14u] = ' ';
+    bytes[15u] = ' ';
+
+    bytes[16u] = 's';
+    bytes[17u] = 'e';
+    bytes[18u] = 'a';
+    bytes[19u] = 'r';
+    bytes[20u] = 'c';
+    bytes[21u] = 'h';
+    bytes[22u] = '_';
+    bytes[23u] = 'i';
+    bytes[24u] = 'd';
+    bytes[25u] = '=';
+
+    bytes[26u] = '[';
+    bytes[27u] = random_str[0];
+    bytes[28u] = random_str[1];
+    bytes[29u] = random_str[2];
+    bytes[30u] = random_str[3];
+    bytes[31u] = ']';
+
+    for (idx = 32u; idx < 13u * 4u - 1u; idx++)
         bytes[idx] = ' ';
     //
     // mandatory termination
@@ -47,19 +77,17 @@ static void deti_coins_cpu_avx512_search(void)
         //
         // interleave data for AVX-512 processing
         //
-        for(idx = 0u; idx < 13u; idx++){
-            interleaved_data[16u*idx] = coin[idx];
-        }
-        for(idx = 1u; idx < 16u;idx++){
-            for(n = 0u; n < 13u; n++){
-            interleaved_data[16u * n + idx] = interleaved_data[16u * n];
+        for(idx = 0;idx < 16;idx++){
+            for(n = 0;n < 12;n++){
+                interleaved_coins[16*n+idx] = coin[n];
             }
+            interleaved_coins[16*n+idx] = coin[n] + idx; // change last block of the coin to introduce variability
         }
 
         //
         // compute MD5 hash using AVX-512
         //
-        md5_cpu_avx512((v16si *)interleaved_data, (v16si *)interleaved_hash);  
+        md5_cpu_avx512((v16si *)interleaved_coins, (v16si *)interleaved_hash);  
         for(idx = 0u ; idx < 16u; idx++){
             for(n = 0u; n<4u; n++){
                 hash[n] = interleaved_hash[16u*n + idx];
@@ -78,7 +106,9 @@ static void deti_coins_cpu_avx512_search(void)
             //
             if (n >= 32u)
             {
+                coin[12] += idx;
                 save_deti_coin(coin);
+                coin[12] -= idx;
                 n_coins++;
             }
 
